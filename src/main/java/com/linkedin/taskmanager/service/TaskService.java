e19678e461e1b805207c4926b5fc5876357670be
rashid <rashidrizan8@gamil.com>
2025-04-06 23:29:51 +0530

    added the service layer remaining test scripts

--- src/main/java/com/linkedin/taskmanager/service/TaskService.java
+++ src/main/java/com/linkedin/taskmanager/service/TaskService.java
@@ -1,8 +1,27 @@
 package com.linkedin.taskmanager.service;
 
+import com.linkedin.taskmanager.model.Task;
 import com.linkedin.taskmanager.repository.TaskRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class TaskService {
 
-    private TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task updateTaskStatus(Long id, String status){
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()){
+            Task t= task.get();
+            t.setStatus(status);
+            return taskRepository.save(t);
+        }
+        return null;
+    }
 }
